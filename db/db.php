e603ed764ebc3c0d75aca7753775d99a198d9f84
AdamYa <adam.yahid@gmail.com>
2015-01-25 16:25:49 +0200

    change personal details to php, add form token to dispute form, add DB connection file

---
+++ db/db.php
@@ -0,0 +1,17 @@
+$servername = "182.50.131.14";//"localhost";
+$username = "mtastudDB1";//"root";
+$password = "mtastudDB1!";//"root";
+$dbname = "mtastudDB1";//"shaming";
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "shaming";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+$conn->set_charset("utf8");
+// Check connection
+if ($conn->connect_error) 
+{
+	die("Connection failed: " . $conn->connect_error);
+} 
--- shaming_list.php
+++ shaming_list.php
@@ -75,26 +75,7 @@
 					<section class="mainShaming">
 						<?php
 
-<<<<<<< HEAD
-					$servername = "182.50.131.14";//"localhost";
-					$username = "mtastudDB1";//"root";
-					$password = "mtastudDB1!";//"root";
-					$dbname = "mtastudDB1";//"shaming";
-=======
-							$servername = "localhost";
-							$username = "root";
-							$password = "root";
-							$dbname = "shaming";
->>>>>>> FETCH_HEAD
-
-							// Create connection
-							$conn = new mysqli($servername, $username, $password, $dbname);
-							$conn->set_charset("utf8");
-							// Check connection
-							if ($conn->connect_error) 
-							{
-							    die("Connection failed: " . $conn->connect_error);
-							} 
+							include("db/db.php");
 
 							$sql = "SELECT * FROM list";
 							$result = $conn->query($sql);
